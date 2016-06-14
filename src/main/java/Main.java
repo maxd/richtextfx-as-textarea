e3ca73fb538c0918cdc2924d461df01806212eef
Maxim Dobryakov <mdobryakov@spbtv.com>
2016-06-14 12:47:16 +0300

    Initial commit

---
+++ src/main/java/Main.java
@@ -0,0 +1,41 @@
+import javafx.application.Application;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.VBox;
+import javafx.stage.Stage;
+import org.fxmisc.flowless.VirtualizedScrollPane;
+import org.fxmisc.richtext.CodeArea;
+
+public class Main extends Application {
+
+    public void start(Stage primaryStage) throws Exception {
+        CodeArea codeArea = new CodeArea(longText());
+
+        VirtualizedScrollPane codeAreaScroll = new VirtualizedScrollPane<>(codeArea);
+
+        TextArea textArea = new TextArea(longText());
+
+        VBox placeholder = new VBox();
+        placeholder.getChildren().addAll(codeAreaScroll, textArea);
+        placeholder.setSpacing(14);
+        placeholder.setPadding(new Insets(14));
+
+        Scene scene = new Scene(placeholder);
+        scene.getStylesheets().add("Main.css");
+
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    private String longText() {
+        StringBuilder stringBuilder = new StringBuilder();
+
+        for (int i = 0; i < 50; i++) {
+            stringBuilder.append("line-").append(i).append(System.lineSeparator());
+        }
+
+        return stringBuilder.toString();
+    }
+
+}
